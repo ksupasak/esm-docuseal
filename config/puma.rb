a566abcabbd2379dcaf5d8ca89f9a6c34c4914f7
Supasak Kulawonganunchai <ksupasak@gmail.com>
2025-01-25 23:02:51 +0700

    update

--- config/puma.rb
+++ config/puma.rb
@@ -17,9 +17,18 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
+
+ssl_bind 'app', '3000', {
+  key: "config/ssl/server.key",
+  cert: "config/ssl/server.crt"
+}
+
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch('PORT', 3000)
+port ENV.fetch('PORT', 3001)
+
+
 
 # Specifies the `environment` that Puma will run in.
 #
